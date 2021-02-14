845df790906d35f24f463c66d75a5931de0ae393
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-14 18:11:50 -0300

    creates a test that verifies if a beer was succesfully created

---
+++ src/test/java/com/digitalinnovation/one/beerstock/service/BeerServiceTest.java
@@ -0,0 +1,56 @@
+package com.digitalinnovation.one.beerstock.service;
+
+import com.digitalinnovation.one.beerstock.builder.BeerDTOBuilder;
+import com.digitalinnovation.one.beerstock.dto.BeerDTO;
+import com.digitalinnovation.one.beerstock.entity.Beer;
+import com.digitalinnovation.one.beerstock.exception.BeerAlreadyRegisteredException;
+import com.digitalinnovation.one.beerstock.mapper.BeerMapper;
+import com.digitalinnovation.one.beerstock.repository.BeerRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerServiceTest {
+
+    private static final long INVALID_BEER_ID = 1L;
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedThenItShouldBeCreated() throws BeerAlreadyRegisteredException {
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer expectedSavedBeer = beerMapper.toModel(beerDTO);
+
+        when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+
+        BeerDTO createdBeerDTO =  beerService.createBeer(beerDTO);
+
+        //hamcrest, MatcherAssert, Matchers
+        assertThat(createdBeerDTO.getId(), is(equalTo(beerDTO.getId())));
+        assertThat(createdBeerDTO.getName(), is(equalTo(beerDTO.getName())));
+        assertThat(createdBeerDTO.getQuantity(), is(equalTo(beerDTO.getQuantity())));
+
+        //assertEquals(beerDTO.getId(), createdBeerDTO.getId());
+        //assertEquals(beerDTO.getName(), createdBeerDTO.getName())
+    }
+}
